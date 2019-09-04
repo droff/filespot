3d1a7edd65442f660a51758bfb9a7c557efff7f1
droff <adroff@gmail.com>
2019-09-04 14:30:54 +0300

    objects: GET, DELETE

--- objects.go
+++ objects.go
@@ -9,6 +9,8 @@ const objectsBasePath = "/1/objects"
 
 type ObjectsService interface {
 	List(context.Context) ([]Object, *http.Response, error)
+	Get(context.Context, string) (*Object, *http.Response, error)
+	Delete(context.Context, string) (*http.Response, error)
 }
 
 type ObjectsCli struct {
@@ -16,24 +18,69 @@ type ObjectsCli struct {
 }
 
 type Object struct {
-	ID           string `json:"id"`
-	Name         string `json:"name"`
-	Path         string `json:"path"`
-	IsDir        bool   `json:"is_dir"`
-	Size         uint32 `json:"size"`
-	ContentType  string `json:"content_type"`
-	CreateDate   string `json:"create_date"`
-	LatestUpdate string `json:"latest_update"`
-	ResourceURL  string `json:"resource_url"`
-	CDNURL       string `json:"cdn_url"`
-	VODHLS       string `json:"vod_hls"`
-	Video        string `json:"video"`
-	Private      bool   `json:"private"`
-	Status       string `json:"status"`
+	ID           string          `json:"id"`
+	Name         string          `json:"name"`
+	Path         string          `json:"path"`
+	IsDir        bool            `json:"is_dir"`
+	Size         uint32          `json:"size"`
+	ContentType  string          `json:"content_type"`
+	CreateDate   string          `json:"create_date"`
+	LatestUpdate string          `json:"latest_update"`
+	ResourceURL  string          `json:"resource_url"`
+	CDNURL       string          `json:"cdn_url"`
+	VODHLS       string          `json:"vod_hls"`
+	Video        string          `json:"video"`
+	Private      bool            `json:"private"`
+	Status       string          `json:"status"`
+	Advanced     *ObjectAdvanced `json:"advanced"`
+	Previews     []string        `json:"previews"`
+	Description  string          `json:"description"`
+}
+
+type ObjectAdvanced struct {
+	AudioStreams []ObjectAudioStream `json:"audio_streams"`
+	Format       *ObjectFormat       `json:"format"`
+	VideoStreams []ObjectVideoStream `json:"video_streams"`
+}
+
+type ObjectAudioStream struct {
+	BitRate       uint32  `json:"bit_rate"`
+	ChannelLayout string  `json:"channel_layout"`
+	Channels      uint32  `json:"channels"`
+	CodecLongName string  `json:"codec_long_name"`
+	CodecType     string  `json:"codec_type"`
+	Duration      float32 `json:"duration"`
+	Index         uint32  `json:"index"`
+	SampleRate    uint32  `json:"sample_rate"`
+}
+
+type ObjectFormat struct {
+	BitRate        uint32  `json:"bit_rate"`
+	Duration       float32 `json:"duration"`
+	FormatLongName string  `json:"format_long_name"`
+	FormatName     string  `json:"format_name"`
+	NBStreams      uint32  `json:"nb_streams"`
+}
+
+type ObjectVideoStream struct {
+	BitRate            uint32  `json:"bit_rate"`
+	CodecName          string  `json:"codec_name"`
+	CodecType          string  `json:"codec_type"`
+	CodecLongName      string  `json:"codeclongname"`
+	DisplayAspectRatio string  `json:"display_aspect_ratio"`
+	Duration           float32 `json:"duration"`
+	FPS                float32 `json:"fps"`
+	Height             uint32  `json:"height"`
+	Index              uint32  `json:"index"`
+	Width              uint32  `json:"width"`
 }
 
 type objectsRoot struct {
 	Objects []Object `json:"objects"`
+}
+
+type objectRoot struct {
+	Object *Object `json:"object"`
 }
 
 func (c ObjectsCli) List(ctx context.Context) ([]Object, *http.Response, error) {
@@ -45,8 +92,42 @@ func (c ObjectsCli) List(ctx context.Context) ([]Object, *http.Response, error) 
 	data := new(objectsRoot)
 	resp, err := c.client.Do(ctx, req, data)
 	if err != nil {
-		return nil, nil, err
+		return nil, resp, err
 	}
 
 	return data.Objects, resp, err
+}
+
+func (c ObjectsCli) Get(ctx context.Context, id string) (*Object, *http.Response, error) {
+	endpointURL := objectsBasePath + "/" + id
+
+	req, err := c.client.NewRequest(ctx, http.MethodGet, endpointURL, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	data := new(objectRoot)
+	resp, err := c.client.Do(ctx, req, data)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return data.Object, resp, err
+}
+
+func (c ObjectsCli) Delete(ctx context.Context, id string) (*http.Response, error) {
+	endpointURL := objectsBasePath + "/" + id
+
+	req, err := c.client.NewRequest(ctx, http.MethodDelete, endpointURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	data := &struct{}{}
+	resp, err := c.client.Do(ctx, req, data)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, err
 }
--- objects_test.go
+++ objects_test.go
@@ -12,8 +12,9 @@ func TestObjectsList(t *testing.T) {
 	defer shutdown()
 
 	mux.HandleFunc("/1/objects", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			t.Errorf("Objects List request method = %v, expected %v", r.Method, http.MethodGet)
+		m := http.MethodGet
+		if r.Method != m {
+			t.Errorf("Objects List request method = %v, expected %v", r.Method, m)
 		}
 
 		fmt.Fprintf(w, `{
@@ -96,5 +97,89 @@ func TestObjectsList(t *testing.T) {
 
 	if !reflect.DeepEqual(objects, expected) {
 		t.Errorf("Objects.List = %v, expected %v", objects, expected)
+	}
+}
+
+func TestObjectsGet(t *testing.T) {
+	setup()
+	defer shutdown()
+
+	mux.HandleFunc("/1/objects/56787f0c044dfe226b000001", func(w http.ResponseWriter, r *http.Request) {
+		m := http.MethodGet
+		if r.Method != m {
+			t.Errorf("Objects List request method = %v, expected %v", r.Method, m)
+		}
+
+		fmt.Fprintf(w, `{
+            "object": {
+                "id": "56787f0c044dfe226b000001",
+                "name": "test.mp4",
+                "path": "/test.mp4",
+                "is_dir": false,
+                "size": 985781,
+                "content_type": "video/mp4",
+                "create_date": "22.12.2015T01:37:00",
+                "latest_update": "",
+                "resource_url": "api.platformcraft.ru/objects/56787f0c044dfe226b000001",
+                "cdn_url": "cdn.platformcraft.ru/billy/test.mp4",
+                "vod_hls": "customer.cdn.ru/customer-vod/_definst_/mp4:billy/test.mp4/playlist.m3u8",
+                "video": "video.platformcraft.ru/56787f0c044dfe226b000001",
+                "private": true,
+                "status": "ok"
+            }
+        }`)
+	})
+
+	object, _, err := client.Objects.Get(ctx, "56787f0c044dfe226b000001")
+	if err != nil {
+		t.Errorf("Objects.Get returned error: %v", err)
+	}
+
+	expected := &Object{
+		ID:           "56787f0c044dfe226b000001",
+		Name:         "test.mp4",
+		Path:         "/test.mp4",
+		IsDir:        false,
+		Size:         985781,
+		ContentType:  "video/mp4",
+		CreateDate:   "22.12.2015T01:37:00",
+		LatestUpdate: "",
+		ResourceURL:  "api.platformcraft.ru/objects/56787f0c044dfe226b000001",
+		CDNURL:       "cdn.platformcraft.ru/billy/test.mp4",
+		VODHLS:       "customer.cdn.ru/customer-vod/_definst_/mp4:billy/test.mp4/playlist.m3u8",
+		Video:        "video.platformcraft.ru/56787f0c044dfe226b000001",
+		Private:      true,
+		Status:       "ok",
+	}
+
+	if !reflect.DeepEqual(object, expected) {
+		t.Errorf("Objects.Get = %v, expected %v", object, expected)
+	}
+}
+
+func TestObjectsDelete(t *testing.T) {
+	setup()
+	defer shutdown()
+
+	mux.HandleFunc("/1/objects/56787f0c044dfe226b000001", func(w http.ResponseWriter, r *http.Request) {
+		m := http.MethodDelete
+		if r.Method != m {
+			t.Errorf("Objects List request method = %v, expected %v", r.Method, m)
+		}
+
+		fmt.Fprintf(w, `{
+            "code": 200,
+            "status": "success"
+        }`)
+	})
+
+	resp, err := client.Objects.Delete(ctx, "56787f0c044dfe226b000001")
+	if err != nil {
+		t.Errorf("Objects.Delete returned error: %v", err)
+	}
+
+	expected := http.StatusOK
+	if resp.StatusCode != expected {
+		fmt.Errorf("Objects.Delete request code = %v, expected %v", resp.StatusCode, expected)
 	}
 }
