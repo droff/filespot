801db94f44622cdc2254fb4128b6d7cfd9deff6e
droff <adroff@gmail.com>
2020-05-13 22:43:10 +0300

    players done

--- players.go
+++ players.go
@@ -10,7 +10,11 @@ const playersBasePath = "/1/players"
 // PlayersService implements interface with API /players endpoint.
 // See https://doc.platformcraft.ru/filespot/api/en/#players
 type PlayersService interface {
+	List(context.Context, interface{}) (*playersRoot, *http.Response, error)
+	Get(context.Context, string) (*Player, *http.Response, error)
 	Create(context.Context, *PlayerCreateRequest) (*Player, *http.Response, error)
+	Update(context.Context, string, *PlayerUpdateRequest) (*http.Response, error)
+	Delete(context.Context, string) (*http.Response, error)
 }
 
 // PlayersCli handles communication with API
@@ -38,6 +42,11 @@ type Player struct {
 type Videos map[string]string
 type Tags []string
 
+// playersRoot respresents a List root
+type playersRoot struct {
+	Players []Player `json:"players"`
+}
+
 // playerRoot represents a Get root
 type playerRoot struct {
 	Player *Player `json:"player"`
@@ -55,6 +64,56 @@ type PlayerCreateRequest struct {
 	Geo          Geo    `json:"geo"`
 }
 
+// PlayerUpdateRequest indentifies Player for the Update request
+type PlayerUpdateRequest struct {
+	Name         string `json:"name"`
+	Folder       string `json:"folder"`
+	Videos       Videos `json:"videos"`
+	ScreenShotID string `json:"screen_shot_id"`
+	Description  string `json:"description"`
+	Tags         Tags   `json:"tags"`
+	Geo          Geo    `json:"geo"`
+}
+
+// List of Players
+func (c PlayersCli) List(ctx context.Context, params interface{}) (*playersRoot, *http.Response, error) {
+	path, err := addParams(playersBasePath, params)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := c.client.NewRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	data := new(playersRoot)
+	resp, err := c.client.Do(ctx, req, data)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return data, resp, err
+}
+
+// Get Player
+func (c PlayersCli) Get(ctx context.Context, id string) (*Player, *http.Response, error) {
+	endpointURL := playersBasePath + "/" + id
+
+	req, err := c.client.NewRequest(ctx, http.MethodGet, endpointURL, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	data := new(playerRoot)
+	resp, err := c.client.Do(ctx, req, data)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return data.Player, resp, err
+}
+
 // Create Player
 func (c PlayersCli) Create(ctx context.Context, playerCreateRequest *PlayerCreateRequest) (*Player, *http.Response, error) {
 	req, err := c.client.NewRequest(ctx, http.MethodPost, playersBasePath, playerCreateRequest)
@@ -69,4 +128,37 @@ func (c PlayersCli) Create(ctx context.Context, playerCreateRequest *PlayerCreat
 	}
 
 	return data.Player, resp, err
+}
+
+// Update Player
+func (c PlayersCli) Update(ctx context.Context, id string, playerUpdateRequest *PlayerUpdateRequest) (*http.Response, error) {
+	endpointURL := playersBasePath + "/" + id
+
+	req, err := c.client.NewRequest(ctx, http.MethodPut, endpointURL, playerUpdateRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	data := &struct{}{}
+	resp, err := c.client.Do(ctx, req, data)
+
+	return resp, err
+}
+
+// Delete Player
+func (c PlayersCli) Delete(ctx context.Context, id string) (*http.Response, error) {
+	endpointURL := playersBasePath + "/" + id
+
+	req, err := c.client.NewRequest(ctx, http.MethodDelete, endpointURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	data := &struct{}{}
+	resp, err := c.client.Do(ctx, req, data)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, err
 }
--- players_test.go
+++ players_test.go
@@ -7,6 +7,142 @@ import (
 	"testing"
 )
 
+func TestPlayersList(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/1/players", func(w http.ResponseWriter, r *http.Request) {
+		m := http.MethodGet
+		if r.Method != m {
+			t.Errorf("PLayers.List request method = %v, expected %v", r.Method, m)
+		}
+
+		fmt.Fprintf(w, `{
+            "code": 200,
+            "status": "success",
+            "count": 4,
+            "count_on_page": 2,
+            "players": [
+                {
+                    "id": "567d3643534b4474087c221e",
+                    "name": "player_name_1",
+                    "path": "/player_name_1",
+                    "is_dir": false,
+                    "videos": {
+                        "360": "cdn.platformcraft.ru/alex/example_360.mp4",
+                        "480": "cdn.platformcraft.ru/alex/example_480.mp4"
+                    },
+                    "screen_shot_url": "cdn.platformcraft.ru/alex/example.jpg",
+                    "vast_ad_tag_url": "http://example.com/example-vast.xml",
+                    "create_date": "25.12.2015T15:27:48",
+                    "href": "video.platformcraft.ru/embed/567d3643534b4474087c221e",
+                    "frame_tag": "<iframe width=\"558\" height=\"264\" src=\"video.platformcraft.ru/embed/567d3643534b4474087c221e\" frameBorder=\"0\" scrolling=\"no\" allowFullScreen></iframe>",
+                    "description": "test player",
+                    "tags": [],
+                    "geo": null
+                }
+            ],
+            "paging": {
+                "next": "api.platformcraft.ru/1/players?pagingts=1525858519&limit=2&start=2",
+                "prev": null
+            }
+        }`)
+	})
+
+	expected := []Player{
+		{
+			ID:    "567d3643534b4474087c221e",
+			Name:  "player_name_1",
+			Path:  "/player_name_1",
+			IsDir: false,
+			Videos: Videos{
+				"360": "cdn.platformcraft.ru/alex/example_360.mp4",
+				"480": "cdn.platformcraft.ru/alex/example_480.mp4",
+			},
+			ScreenShotURL: "cdn.platformcraft.ru/alex/example.jpg",
+			VastAdTagURL:  "http://example.com/example-vast.xml",
+			CreateDate:    "25.12.2015T15:27:48",
+			Href:          "video.platformcraft.ru/embed/567d3643534b4474087c221e",
+			FrameTag:      "<iframe width=\"558\" height=\"264\" src=\"video.platformcraft.ru/embed/567d3643534b4474087c221e\" frameBorder=\"0\" scrolling=\"no\" allowFullScreen></iframe>",
+			Description:   "test player",
+			Tags:          Tags{},
+			Geo:           nil,
+		},
+	}
+
+	root, _, err := client.Players.List(ctx, nil)
+	if err != nil {
+		t.Errorf("Players.List returned error: %v", err)
+	}
+
+	if !reflect.DeepEqual(root.Players, expected) {
+		t.Errorf("Players.List = %v, expected %v", root.Players, expected)
+	}
+}
+
+func TestPlayersGet(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/1/players/567d3643534b4474087c221e", func(w http.ResponseWriter, r *http.Request) {
+		m := http.MethodGet
+		if r.Method != m {
+			t.Errorf("Players.Get request method = %v, expected %v", r.Method, m)
+		}
+
+		fmt.Fprintf(w, `{
+            "code": 200,
+            "status": "success",
+            "player": {
+                "id": "567d3643534b4474087c221e",
+                "name": "player_name_1",
+                "path": "/player_name_1",
+                "is_dir": false,
+                "videos": {
+                    "360": "cdn.platformcraft.ru/alex/example_360.mp4",
+                    "480": "cdn.platformcraft.ru/alex/example_480.mp4"
+                },
+                "screen_shot_url": "cdn.platformcraft.ru/alex/example.jpg",
+                "vast_ad_tag_url": "http://example.com/example-vast.xml",
+                "create_date": "25.12.2015T15:27:48",
+                "href": "video.platformcraft.ru/embed/567d3643534b4474087c221e",
+                "frame_tag": "<iframe width=\"558\" height=\"264\" src=\"video.platformcraft.ru/embed/567d3643534b4474087c221e\" frameBorder=\"0\" scrolling=\"no\" allowFullScreen></iframe>",
+                "description": "test player",
+                "tags": [],
+                "geo": null
+            }
+        }`)
+	})
+
+	expected := &Player{
+		ID:    "567d3643534b4474087c221e",
+		Name:  "player_name_1",
+		Path:  "/player_name_1",
+		IsDir: false,
+		Videos: Videos{
+			"360": "cdn.platformcraft.ru/alex/example_360.mp4",
+			"480": "cdn.platformcraft.ru/alex/example_480.mp4",
+		},
+		ScreenShotURL: "cdn.platformcraft.ru/alex/example.jpg",
+		VastAdTagURL:  "http://example.com/example-vast.xml",
+		CreateDate:    "25.12.2015T15:27:48",
+		Href:          "video.platformcraft.ru/embed/567d3643534b4474087c221e",
+		FrameTag:      "<iframe width=\"558\" height=\"264\" src=\"video.platformcraft.ru/embed/567d3643534b4474087c221e\" frameBorder=\"0\" scrolling=\"no\" allowFullScreen></iframe>",
+		Description:   "test player",
+		Tags:          Tags{},
+		Geo:           nil,
+	}
+
+	player, _, err := client.Players.Get(ctx, "567d3643534b4474087c221e")
+	if err != nil {
+		t.Errorf("Players.Get returned error: %v", err)
+	}
+
+	if !reflect.DeepEqual(player, expected) {
+		t.Errorf("Players.Get = %v, expected %v", player, expected)
+	}
+}
+
 func TestPlayersCreate(t *testing.T) {
 	setup()
 	defer teardown()
@@ -89,5 +225,63 @@ func TestPlayersCreate(t *testing.T) {
 
 	if !reflect.DeepEqual(player, expected) {
 		t.Errorf("Players.Create = %v, expected %v", player, expected)
+	}
+}
+
+func TestPlayerUpdate(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/1/players/567d3643534b4474087c221d", func(w http.ResponseWriter, r *http.Request) {
+		m := http.MethodPut
+		if r.Method != m {
+			t.Errorf("Players.Update request method = %v, expected %v", r.Method, m)
+		}
+
+		fmt.Fprintf(w, `{
+            "code": 200,
+            "status": "success"
+        }`)
+	})
+
+	playerUpdateRequest := &PlayerUpdateRequest{
+		Name:        "new_player_name",
+		Description: "this description was updated",
+	}
+	resp, err := client.Players.Update(ctx, "567d3643534b4474087c221d", playerUpdateRequest)
+	if err != nil {
+		t.Errorf("Players.Update returned error: %v", err)
+	}
+
+	expected := http.StatusOK
+	if resp.StatusCode != expected {
+		t.Errorf("Players.Update request code = %v, expected %v", resp.StatusCode, expected)
+	}
+}
+
+func TestPlayersDelete(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/1/players/567d3643534b4474087c221d", func(w http.ResponseWriter, r *http.Request) {
+		m := http.MethodDelete
+		if r.Method != m {
+			t.Errorf("Players.Delete request method = %v, expected %v", r.Method, m)
+		}
+
+		fmt.Fprintf(w, `{
+            "code": 200,
+            "status": "success"
+        }`)
+	})
+
+	resp, err := client.Players.Delete(ctx, "567d3643534b4474087c221d")
+	if err != nil {
+		t.Errorf("Players.Delete returned error: %v", err)
+	}
+
+	expected := http.StatusOK
+	if resp.StatusCode != expected {
+		t.Errorf("Players.Delete request code = %v, expected %v", resp.StatusCode, expected)
 	}
 }
